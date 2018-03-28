6c9f075c079341df3b0ba9a722e7bae94a4cb5d6
Guest User <Guest@epicodus-7.local>
2018-03-28 16:42:10 -0700

    add logo; add employee login/logout buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,16 @@ export class AppComponent {
     new Keg('Beer1', 'Brand1', 3, 4),
     new Keg('Beer2', 'Brand2', 3, 4),
     new Keg('Beer3', 'Brand2', 3, 4),
-    new Keg('Beer4', 'Brand1', 3, 4)
-  ]
+    new Keg('Beer4', 'Brand1', 3, 4),
+    new Keg('Beer5', 'Brand2', 3, 4),
+    new Keg('Beer6', 'Brand3', 3, 4),
+    new Keg('Beer7', 'Brand3', 3, 4)
+  ];
+  employee = false;
+  employeeLogin() {
+    this.employee = true;
+  }
+  employeeLogout() {
+    this.employee = false;
+  }
 }
