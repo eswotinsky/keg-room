7b7dddb595760a32639a4c9f7a4715344617772c
Eric Swotinsky and Tyler Kostelak <eric+tyler@>
2018-03-29 10:31:20 -0700

    add Keg pintCount property; add order button to cards.

--- src/app/models/keg.model.ts
+++ src/app/models/keg.model.ts
@@ -1,3 +1,18 @@
 export class Keg {
+  public pintCount: number = 124;
   constructor(public name: string, public brand: string, public price: number, public alcoholContent: number) {}
+
+  orderPint() {
+    this.pintCount--;
+    console.log(`${this.name}: ${this.pintCount} pints remaining.`)
+  }
+
+  colorCode() {
+    if (this.pintCount < 10) {
+      return "bg-danger";
+    }
+    else {
+      return "bg-info";
+    }
+  }
 }
